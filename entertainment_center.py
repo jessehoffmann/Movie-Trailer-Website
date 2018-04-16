98fa25294733873444d8937af75bfc2a3d5e25a1
Jesse <hoffmann.jesse@gmail.com>
2018-04-16 15:47:13 -0700

    modified:   entertainment_center.py 	modified:   media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,28 +1,45 @@
+"""This file creates objects for the specific movies."""
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story", "A story of a boy and his toys that come to life.",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=v-PjgYDrg70")
+A_QUIET_PLACE = media.Movie("A Quiet Place", "In a post-apocalyptic world a " \
+                            "family tries to survive without making a sound.",
+                            "https://upload.wikimedia.org/wikipedia/en/a/a0/" \
+                            "A_Quiet_Place_film_poster.png",
+                            "https://www.youtube.com/watch?v=WR7cc5t7tv8")
 #print(toy_story.storyline)
-avatar = media.Movie("Avatar", "Humans visit an alien world in an attempt to steal the planet's resources.",
-                    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-                    "https://www.youtube.com/watch?v=d1_JBMrrYw8")
+AVATAR = media.Movie("Avatar", "Humans visit an alien world in an attempt to" \
+                     "steal the planet's resources.",
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/"\
+                     "Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=d1_JBMrrYw8")
 #avatar.show_trailer()
-good_will_hunting = media.Movie("Good Will Hunting", "A young genius struggles to find his place in the world.",
-                                "https://upload.wikimedia.org/wikipedia/en/5/52/Good_Will_Hunting.png",
+GOOD_WILL_HUNTING = media.Movie("Good Will Hunting", "A young genius struggles"
+                                "to find his place in the world.",
+                                "https://upload.wikimedia.org/wikipedia/en/5/"\
+                                "52/Good_Will_Hunting.png",
                                 "https://www.youtube.com/watch?v=PaZVjZEFkRs")
 #good_will_hunting.show_trailer()
-blue_is_the_warmest_color = media.Movie("Blue Is The Warmest Color", "A Frech teen forms a deep emotional bond with an older art student",
-                                        "https://upload.wikimedia.org/wikipedia/en/3/3e/La_Vie_d%27Ad%C3%A8le_film_poster.png",
-                                        "https://www.youtube.com/watch?v=Y2OLRrocn3s")
-kung_fu_panda = media.Movie("Kung Fu Panda", "A panda learns kung fu in the hopes of saving his town.",
-                            "https://upload.wikimedia.org/wikipedia/en/7/76/Kungfupanda.jpg",
+BLUE_IS_THE_WARMEST_COLOR = media.Movie("Blue Is The Warmest Color",
+                                        "A Frech teen forms a deep emotional"\
+                                        " bond with an older art student",
+                                        "https://upload.wikimedia.org/"\
+                                        "wikipedia/en/3/3e/La_Vie_d%27Ad%C3%A"\
+                                        "8le_film_poster.png",
+                                        "https://www.youtube.com/watch?v=Y2"\
+                                        "OLRrocn3s")
+KUNG_FU_PANDA = media.Movie("Kung Fu Panda", "A panda learns kung fu in the"\
+                            "hopes of saving his town.",
+                            "https://upload.wikimedia.org/wikipedia/en/7/76/"\
+                            "Kungfupanda.jpg",
                             "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
-oneeighty_degrees_south = media.Movie("180 Degrees South", "A man travels to South America for the journey of a lifetime.",
-                                    "https://upload.wikimedia.org/wikipedia/en/7/70/180_South.jpg",
-                                    "https://www.youtube.com/watch?v=NK3TOHLFL50")
-movies = [toy_story, avatar, good_will_hunting, blue_is_the_warmest_color, kung_fu_panda, oneeighty_degrees_south]
-#fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
+ONEEIGHTY_DEGREE_SOUTH = media.Movie("180 Degrees South", "A man travels to"\
+                                     "South America for the journey of a "\
+                                     "lifetime.",
+                                     "https://upload.wikimedia.org/wikipedia"\
+                                     "/en/7/70/180_South.jpg",
+                                     "https://www.youtube.com/watch?v=NK3TOH"\
+                                     "LFL50")
+MOVIES = [A_QUIET_PLACE, AVATAR, GOOD_WILL_HUNTING, BLUE_IS_THE_WARMEST_COLOR,
+          KUNG_FU_PANDA, ONEEIGHTY_DEGREE_SOUTH]
+fresh_tomatoes.open_movies_page(MOVIES)
--- media.py
+++ media.py
@@ -1,31 +1,21 @@
+"""This file creates a class for the movie trailer website."""
 import webbrowser
 import urllib
 
-class Video():
-    def __init__(self, title, duration):
-        self.title = title
-        self.duration = duration
-
-class Movie(Video):
-    """This class provides a way to store movie related information"""
+class Movie(object):
+    """This class provides a way to store movie related information."""
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-        Video.__init__(self, title, duration)
+#        Video.__init__(self, title, duration)
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
     def show_trailer(self):
+        """This function open the trailer."""
         webbrowser.open(self.trailer_youtube_url)
     def open_poster(self):
+        """This function downloads the poster image"""
         urllib.urlretrieve(self.poster_image_url, "01.jpg")
-
-class TvShow(Video):
-    def __init__(self, season, episode, tv_station):
-        Video.__init__(self, title, duration)
-        self.season = season
-        self.episode = episode
-        self.tv_station = tv_station
-    def get_local_listing():
