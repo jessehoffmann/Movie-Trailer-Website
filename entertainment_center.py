cd524c13f20e49792d06113687dc583b83e39178
Jesse <hoffmann.jesse@gmail.com>
2018-04-21 20:11:55 -0700

    modified:   entertainment_center.py 	modified:   media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,43 +2,40 @@
 import media
 import fresh_tomatoes
 
-A_QUIET_PLACE = media.Movie("A Quiet Place", "In a post-apocalyptic world a " \
+A_QUIET_PLACE = media.Movie("A Quiet Place", "In a post-apocalyptic world a "
                             "family tries to survive without making a sound.",
-                            "https://upload.wikimedia.org/wikipedia/en/a/a0/" \
+                            "https://upload.wikimedia.org/wikipedia/en/a/a0/"
                             "A_Quiet_Place_film_poster.png",
                             "https://www.youtube.com/watch?v=WR7cc5t7tv8")
-#print(toy_story.storyline)
-AVATAR = media.Movie("Avatar", "Humans visit an alien world in an attempt to" \
+AVATAR = media.Movie("Avatar", "Humans visit an alien world in an attempt to"
                      "steal the planet's resources.",
-                     "https://upload.wikimedia.org/wikipedia/en/b/b0/"\
+                     "https://upload.wikimedia.org/wikipedia/en/b/b0/"
                      "Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=d1_JBMrrYw8")
-#avatar.show_trailer()
 GOOD_WILL_HUNTING = media.Movie("Good Will Hunting", "A young genius struggles"
                                 "to find his place in the world.",
-                                "https://upload.wikimedia.org/wikipedia/en/5/"\
+                                "https://upload.wikimedia.org/wikipedia/en/5/"
                                 "52/Good_Will_Hunting.png",
                                 "https://www.youtube.com/watch?v=PaZVjZEFkRs")
-#good_will_hunting.show_trailer()
 BLUE_IS_THE_WARMEST_COLOR = media.Movie("Blue Is The Warmest Color",
-                                        "A Frech teen forms a deep emotional"\
+                                        "A Frech teen forms a deep emotional"
                                         " bond with an older art student",
-                                        "https://upload.wikimedia.org/"\
-                                        "wikipedia/en/3/3e/La_Vie_d%27Ad%C3%A"\
+                                        "https://upload.wikimedia.org/"
+                                        "wikipedia/en/3/3e/La_Vie_d%27Ad%C3%A"
                                         "8le_film_poster.png",
-                                        "https://www.youtube.com/watch?v=Y2"\
+                                        "https://www.youtube.com/watch?v=Y2"
                                         "OLRrocn3s")
-KUNG_FU_PANDA = media.Movie("Kung Fu Panda", "A panda learns kung fu in the"\
+KUNG_FU_PANDA = media.Movie("Kung Fu Panda", "A panda learns kung fu in the"
                             "hopes of saving his town.",
-                            "https://upload.wikimedia.org/wikipedia/en/7/76/"\
+                            "https://upload.wikimedia.org/wikipedia/en/7/76/"
                             "Kungfupanda.jpg",
                             "https://www.youtube.com/watch?v=PXi3Mv6KMzY")
-ONEEIGHTY_DEGREE_SOUTH = media.Movie("180 Degrees South", "A man travels to"\
-                                     "South America for the journey of a "\
+ONEEIGHTY_DEGREE_SOUTH = media.Movie("180 Degrees South", "A man travels to"
+                                     "South America for the journey of a "
                                      "lifetime.",
-                                     "https://upload.wikimedia.org/wikipedia"\
+                                     "https://upload.wikimedia.org/wikipedia"
                                      "/en/7/70/180_South.jpg",
-                                     "https://www.youtube.com/watch?v=NK3TOH"\
+                                     "https://www.youtube.com/watch?v=NK3TOH"
                                      "LFL50")
 MOVIES = [A_QUIET_PLACE, AVATAR, GOOD_WILL_HUNTING, BLUE_IS_THE_WARMEST_COLOR,
           KUNG_FU_PANDA, ONEEIGHTY_DEGREE_SOUTH]
--- media.py
+++ media.py
@@ -2,20 +2,42 @@
 import webbrowser
 import urllib
 
+
 class Movie(object):
-    """This class provides a way to store movie related information."""
+    """This class provides a way to store movie related information.
+
+    The class is designed as an outline for storing the data for each
+    individual movie. Data includes movie title, storyline, poster image, and
+    trailer.
+
+    Attributes:
+        VALID_RATINGS (list): List containing possible ratings for each movie.
+
+    Parameters:
+        movie_title : str
+            Instance for title of the movie.
+        movie_storyline : str
+            Instance for description of the movie.
+        poster_image : str
+            Instance for poster image of the movie.
+        trailer_youtube : str
+            Instance for trailer of the movie.
+    """
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
-#        Video.__init__(self, title, duration)
+        """The method represents possible instances for movie data."""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
+
     def show_trailer(self):
-        """This function open the trailer."""
+        """The function opens the trailer."""
         webbrowser.open(self.trailer_youtube_url)
+
     def open_poster(self):
-        """This function downloads the poster image"""
+        """The function downloads the poster image."""
         urllib.urlretrieve(self.poster_image_url, "01.jpg")
